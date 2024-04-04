47c5172bd084ef714f10b36e0488ec7f5d719262
Jakob Graf <jakob.graf@svc.co.at>
2024-04-04 23:38:56 +0200

    feat: suite plugin

--- test_tool/__init__.py
+++ test_tool/__init__.py
@@ -2,9 +2,16 @@
 This is the init file for the test_tool package.
 """
 
+from .base import run_tests
 from .import_plugin import CallType, import_plugin
+from .save import Save, default_save, save
 from .substitute import recursively_replace_variables
 from .utils import DotDict
 
-__all__ = ["CallType", "DotDict", "import_plugin",
-           "recursively_replace_variables"]
+__all__ = [
+    "CallType",
+    "DotDict",
+    "import_plugin",
+    "recursively_replace_variables",
+    "run_tests"
+]
